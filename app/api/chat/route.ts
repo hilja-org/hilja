ac9166b59da89eea3127274b34b34cca3b7dfd25
Dusan Turajlic <dusan.turajlic@cambri.io>
2023-11-11 12:15:42 +0200

    Fix pritter issues

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest } from 'next/server';
-import OpenAI from 'openai';
+import { NextRequest } from "next/server";
+import OpenAI from "openai";
 
 // Create an OpenAI API client (that's edge friendly!)
 const assistantId = process.env.OPENAI_ASSISTANT_ID as string;
@@ -11,35 +11,29 @@ const openai = new OpenAI({
 export const runtime = "edge";
 
 export async function POST() {
-  const thread = await openai.beta.threads.create()
+  const thread = await openai.beta.threads.create();
 
-  const message = await openai.beta.threads.messages.create(
-    thread.id,
-    {
-      role: "user",
-      content: "My back hurts quite bad today."
-    }
-  );
+  await openai.beta.threads.messages.create(thread.id, {
+    role: "user",
+    content: "My back hurts quite bad today.",
+  });
 
-  const run = await openai.beta.threads.runs.create(
-    thread.id,
-    {
-      assistant_id: assistantId,
-      instructions: "The usre knows who you are so there is no need to introduce your self. Focuse on helping the user with their problem."
-    }
-  );
+  const run = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: assistantId,
+    instructions:
+      "The usre knows who you are so there is no need to introduce your self. Focuse on helping the user with their problem.",
+  });
 
-  return new Response(JSON.stringify({runId: run.id, threadId: thread.id}), { status: 200 });
+  return new Response(JSON.stringify({ runId: run.id, threadId: thread.id }), {
+    status: 200,
+  });
 }
 
 export async function GET(req: NextRequest) {
-  const threadId = req.nextUrl.searchParams.get('threadId') as string;
-  const runId = req.nextUrl.searchParams.get('runId') as string;
+  const threadId = req.nextUrl.searchParams.get("threadId") as string;
+  const runId = req.nextUrl.searchParams.get("runId") as string;
 
-  const run = await openai.beta.threads.runs.retrieve(
-    threadId,
-    runId
-  );
+  const run = await openai.beta.threads.runs.retrieve(threadId, runId);
 
   return new Response(JSON.stringify(run), { status: 200 });
 }
